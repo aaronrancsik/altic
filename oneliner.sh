cfcffb60328950999ce7b33bf947c5986b7efcfc
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2020-08-23 22:45:59 +0200

    add specefic folder and instructions

--- oneliner.sh
+++ oneliner.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env sh
 
+function int(){
+    read -p "$1"
+}
+
+int "Remove possible leftover 'aaronbooster' folder."
+rm -rf aaronbooster
+
+int "Create new empty 'aaronbooster' folder."
+mkdir aaronbooster
+
+int "Change into 'aaronbooster' folder."
+cd aaronbooster
+
+int "Download scripts."
 curl --remote-name-all https://raw.githubusercontent.com/aaronrancsik/altic/master/{ali,instructions.txt,setup.sh,step_helper.sh}
+
+int "Set permissions."
 chmod +x {setup,step_helper}.sh
+
+int "Execute the installer."
 ./setup.sh
