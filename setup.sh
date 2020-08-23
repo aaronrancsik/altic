240796f67681254d54f2740502c6be3b977347f9
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2020-08-23 21:32:30 +0200

    Initial state for testing

---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+
+KEYS=us
+
+function int(){
+    read -p "$1"
+}
+
+function loadkeys(){
+    int "Press enter to load keys."
+    echo "loadkeys()"
+    #if [ -n "$KEYS" ]; then loadkeys $KEYS; fi    
+}
+
+function updateClock(){
+    int "Press enter to update clock."
+    echo "updateClock()"
+    #timedatectl set-ntp true
+}
+
+function prepare(){
+    int "Press enter to install tmux."
+    echo "prepare()"
+    #pacman -Syy tmux
+}
+
+function main(){
+    loadkeys
+    updateClock
+    prepare
+    alias next='tmux send-keys -t install:0.1 $'\n''
+    
+    tmux new -s install \; split-window -d './step_helper.sh'
+}
+
+main $@
---
+++ step_helper.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+function send2pane(){
+    tmux send-keys -t install:0.0 "$1"
+}
+
+function int(){
+    read -p "$1"
+}
+
+def_msg="Type 'f' to continue"
+
+
+int "To start execute this: . ali "
+
+filename='instructions.txt'
+exec 4<$filename
+while read -u4 p ; do
+    int "$def_msg"
+    sleep 1
+    send2pane "$p"
+done
+
+int "End loop"
+
+
